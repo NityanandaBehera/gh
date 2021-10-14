2668d253dda5ed3357cbb4b08e66be74a998625d
Nitya_web <nityanandabehera85132@gmail.com>
2021-10-14 14:34:14 +0530

    first commit

---
+++ asuh.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int  i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(i==5&&j==3)
+            printf("🥰");
+            else
+            printf("😊");
+        }
+        printf("\n");
+    }
+}
